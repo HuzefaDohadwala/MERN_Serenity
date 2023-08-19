9499e86bae9917351dc2a9ce88235c7b7d79979b
Kedar Karbele <karbelekedar84@gmail.com>
2023-08-19 16:41:22 +0530

    Solved the sasss error

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -1,30 +1,40 @@
-import React, { useState } from 'react';
-import './Navbar.css';
+import React, { useState } from "react";
+import "./Navbar.css";
 
 const Navbar = () => {
-    const [menuOpen, setMenuOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
-    const handleToggle = () => {
-      setMenuOpen(!menuOpen);
+  const handleToggle = () => {
+    console.log("handleToggle called");
+    setMenuOpen(!menuOpen);
   };
 
   return (
-    <nav className={`navbar ${menuOpen ? 'open' : ''}`}>
+    <nav className={`navbar ${menuOpen ? "open" : ""}`}>
       <div className="brand">
         <img src="logo.png" alt="Brand Logo" />
       </div>
-      <div className={`menu ${menuOpen ? 'open' : ''}`}>
+      <div className={`menu ${menuOpen ? "open" : ""}`}>
         <ul>
-          <li><a href="#">About Us</a></li>
-          <li><a href="#">Careers</a></li>
-          <li><a href="#">Providers</a></li>
+          <li>
+            <a href="#">About Us</a>
+          </li>
+          <li>
+            <a href="#">Careers</a>
+          </li>
+          <li>
+            <a href="#">Providers</a>
+          </li>
         </ul>
         <div className="buttons">
           <button className="btn">Login</button>
           <button className="btn">Find a Therapist</button>
         </div>
       </div>
-      <button className={`hamburger ${menuOpen ? 'open' : ''}`} onClick={handleToggle}>
+      <button
+        className={`hamburger ${menuOpen ? "open" : ""}`}
+        onClick={handleToggle}
+      >
         <span className="bar"></span>
         <span className="bar"></span>
         <span className="bar"></span>
