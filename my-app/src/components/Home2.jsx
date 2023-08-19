f7a92f03cafca0e8139b2cae2b9bdb5df947cb1b
Huzefa Dohadwala <dohadwalahuzefa@gmail.com>
2023-08-19 22:25:35 +0530

    home22

--- my-app/src/components/Home2.jsx
+++ my-app/src/components/Home2.jsx
@@ -5,7 +5,22 @@ const Home2 = () => {
   return (
     <>
     <div className="home2">
-        heloo
+    <div className="heading_home2">
+          <h1>Connect with Diverse Listeners </h1>
+          <h1>Who Share Your Interests!!</h1>
+      </div>
+    <div className="text_home2">
+            <p>Better help is a community that believes access to mental</p>
+            <p>healthcare is essential, so we support anyone with a desire to grow</p>
+    </div>
+    <div className="heading2_home2">
+          <h1>Pick your person </h1>
+      </div>
+    <div className="text2_home2">
+            <p>Find a special place where you can meet</p>
+            <p>new listeners; pick the ones who </p>
+            <p>understand you the best!</p>
+    </div>
     </div>
     </>
   )
