d6bffa2bf5032a80938e3f1fe5cd8560a49158c7
Huzefa Dohadwala <dohadwalahuzefa@gmail.com>
2023-08-19 18:44:44 +0530

    nav and home

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import Home1 from './components/Home1';
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
-     
+     <Home1></Home1>
     </div>
   );
 }
---
+++ my-app/src/components/Home1.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Home1.css"
+
+const Home1 = () => {
+  return (
+    <>
+    <div className="home1">
+        
+    </div>
+    
+    </>
+  )
+}
+
+export default Home1
