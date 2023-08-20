578eb914d4790efb1d8ccd7c644e804ad117c27a
Kedar Karbele <karbelekedar84@gmail.com>
2023-08-21 00:46:29 +0530

    Added footer and Images

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Home1 from "./components/Home1";
 import Home2 from "./components/Home2";
 import Home3 from "./components/Home3";
+import Footer from "./components/Footer";
 
 function App() {
   return (
