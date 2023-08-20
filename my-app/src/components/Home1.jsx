ba5ea86ecf275530d7c3c744df95e7a2bbdbc29f
Kedar Karbele <karbelekedar84@gmail.com>
2023-08-20 23:24:32 +0530

    spell check

--- my-app/src/components/Home1.jsx
+++ my-app/src/components/Home1.jsx
@@ -1,43 +1,59 @@
-import React from 'react'
-import "./Home1.css"
+import React from "react";
+import "./Home1.css";
 
 const Home1 = () => {
   const rows = [
-    { src: 'path_to_your_image.jpg', alt: 'Description', text: 'Instant Connection, Genuine Support' },
-    { src: 'path_to_your_image.jpg', alt: 'Description', text: 'Empathetic Listening, Kind Hearts' },
-    { src: 'path_to_your_image.jpg', alt: 'Description', text: 'Empathetic Listening, Kind Hearts' },
-    { src: 'path_to_your_image.jpg', alt: 'Description', text: 'Compassionate Companions, Always Available' },
-];
-const onButtonClick = () => {
-  console.log("The button was clicked!");
-  // You can add any other logic you want here
-};
+    {
+      src: "path_to_your_image.jpg",
+      alt: "Description",
+      text: "Instant Connection, Genuine Support",
+    },
+    {
+      src: "path_to_your_image.jpg",
+      alt: "Description",
+      text: "Empathetic Listening, Kind Hearts",
+    },
+    {
+      src: "path_to_your_image.jpg",
+      alt: "Description",
+      text: "Empathetic Listening, Kind Hearts",
+    },
+    {
+      src: "path_to_your_image.jpg",
+      alt: "Description",
+      text: "Compassionate Companions, Always Available",
+    },
+  ];
+  const onButtonClick = () => {
+    console.log("The button was clicked!");
+    // You can add any other logic you want here
+  };
 
   return (
     <>
-    <div className="home1"> 
-      <div className="heading">
+      <div className="home1">
+        <div className="heading">
           <h1>Embrace empathy,</h1>
-          <h1>Find your listner</h1>
-      </div>
-      <table className="custom-table">
-            <tbody>
-                {rows.map((row, index) => (
-                    <tr key={index}>
-                        <td className="image-cell">
-                            <img src={row.src} alt={row.alt} className="table-image" />
-                        </td>
-                        <td>{row.text}</td>
-                    </tr>
-                ))}
-            </tbody>
-        </table> 
-        <button className='customButton' onClick={onButtonClick}>
-            Find a Listener
+          <h1>Find your listener</h1>
+        </div>
+        <table className="custom-table">
+          <tbody>
+            {rows.map((row, index) => (
+              <tr key={index}>
+                <td className="image-cell">
+                  <img src={row.src} alt={row.alt} className="table-image" />
+                </td>
+                <td>{row.text}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <button className="customButton" onClick={onButtonClick}>
+          Find a Listener
         </button>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Home1
+export default Home1;
