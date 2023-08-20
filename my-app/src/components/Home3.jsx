8e5dbe8c71be6b13cdf17568116d58947c4f9098
Adityaapandkar <adityaapandkar11@gmail.com>
2023-08-21 00:30:52 +0530

    3rd base final

--- my-app/src/components/Home3.jsx
+++ my-app/src/components/Home3.jsx
@@ -9,11 +9,6 @@ const Home3 = () => {
           <h1>Uncover therapists perfectly</h1>
           <h1>tailored to your journey</h1>
         </div>
-        <div className="text_home3">
-          <p>Explore a world of caring professionals,</p>
-          <p>handpicked to support and guide you through</p>
-          <p>every twist and turn of your life's story.</p>
-        </div>
         <div className="heading2_home3">
           <h1>Connect in your</h1>
           <h1>comfort zone</h1>
