09a56853c408df3c00464a9488c93670a6abadde
Adityaapandkar <adityaapandkar11@gmail.com>
2023-08-21 00:07:43 +0530

    3rd Base Home

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,16 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import Home1 from './components/Home1';
-import Home2 from './components/Home2';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home1 from "./components/Home1";
+import Home2 from "./components/Home2";
+import Home3 from "./components/Home3";
 
 function App() {
   return (
     <div className="App">
-    <Navbar></Navbar>
-    <Home1></Home1>
-    <Home2></Home2>
+      <Navbar></Navbar>
+      <Home1></Home1>
+      <Home2></Home2>
+      <Home3></Home3>
     </div>
   );
 }
---
+++ my-app/src/components/Home3.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Home3.css";
+
+const Home3 = () => {
+  return (
+    <>
+      <div className="home3">
+        <div className="heading_home3">
+          <h1>Uncover therapists perfectly</h1>
+          <h1>tailored to your journey</h1>
+        </div>
+        <div className="text_home3">
+          <p>Explore a world of caring professionals,</p>
+          <p>handpicked to support and guide you through</p>
+          <p>every twist and turn of your life's story.</p>
+        </div>
+        <div className="heading2_home3">
+          <h1>Connect in your</h1>
+          <h1>comfort zone</h1>
+        </div>
+        <div className="text2_home3">
+          <p>You can meet your provider in</p>
+          <p>person or virtually, whichever works</p>
+          <p>best for you.</p>
+        </div>
+        <button className="customButton">Find a Therapist</button>
+      </div>
+    </>
+  );
+};
+
+export default Home3;
