60320e67d7b6804a4c1b58039d7e1304131f8418
Adityaapandkar <adityaapandkar11@gmail.com>
2023-08-25 02:01:17 +0530

    image on disc1

--- my-app/src/components/Disc1.jsx
+++ my-app/src/components/Disc1.jsx
@@ -1,23 +1,32 @@
-import React from 'react'
-import "./Disc1.css"
+import React from "react";
+import "./Disc1.css";
+import disc1image from "./disc1-img.jpg";
 
 const Disc1 = () => {
   return (
     <>
-    <div className="disc1">
-      <div className="heading_disc1"> 
-      <h1 >Unveiling Pathways to</h1>
-      <h1 >Inner Harmony and Growth</h1>
+      <div className="disc1">
+        <div className="disc1_left">
+          <div className="heading_disc1">
+            <h1>Unveiling Pathways to</h1>
+            <h1>Inner Harmony and Growth</h1>
+          </div>
+          <div className="subheading_disc1">
+            <h2>Real Stories, Answers,</h2>
+            <h2>and a Step-by-Step Guide </h2>
+            <h2> to Your Wellness Journey.</h2>
+          </div>
+          <div className="disc1_semic1"></div>
+        </div>
+        <div className="disc1_right">
+          <div class="disc1-image-bg"></div>
+          <div class="disc1-image-lg">
+            <img src={disc1image} alt="Disc 1 Image" />
+          </div>
+        </div>
       </div>
-      <div className="subheading_disc1">
-        <h2>Real Stories, Answers,</h2>
-        <h2>and a Step-by-Step Guide </h2>
-        <h2> to Your Wellness Journey.</h2>
-      </div>
-      <div className="disc1_semic1"></div>
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default Disc1
+export default Disc1;
