3abc98503daef071486d25f7d1d82c7c7eb856a7
Huzefa Dohadwala <dohadwalahuzefa@gmail.com>
2023-08-24 23:06:00 +0530

    desco2

---
+++ my-app/src/components/Disc2.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import "./Disc2.css"
+
+const Disc2 = () => {
+  return (
+    <>
+    <div className="disc2">
+    <div className="disc2_semic11"></div>
+    <div className="disc2_semic12"></div>
+    <div className="disc2_testimony">
+       <div className="test1">
+        <p className="test_words">
+        "For years, I felt trapped in my own thoughts, thinking I was alone in my struggles. Discovering this platform became a beacon of hope. The resources and community here have genuinely made a difference in my life. It's reassuring to know I'm not alone on this journey."
+        </p>
+        <h2 className="test_name">-Alex M., Chicago, IL</h2>
+       </div>
+       <div className="test1">
+        <p className="test_words">
+        "I've always been wary of seeking support online, but this platform changed my perspective. The insightful articles, various therapy options, and connections I've made here have been a game-changer. It's more than a website; it's a community where I feel understood."
+        </p>
+        <h2 className="test_name">-Priya S., Los Angeles, CA</h2>
+       </div>
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default Disc2
--- my-app/src/components/Discover.jsx
+++ my-app/src/components/Discover.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "./Navbar";
 import "./Discover.css";
 import Disc1 from "./Disc1";
+import Disc2 from "./Disc2";
 
 const Discover = () => {
   return (
     <>
       <Navbar></Navbar>
       <Disc1></Disc1>
+      <Disc2></Disc2>
     </>
   );
 };
