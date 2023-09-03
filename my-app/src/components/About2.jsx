3be1a08741ce2e8e8555c82e12b2d4d659df7488
Adityaapandkar <adityaapandkar11@gmail.com>
2023-09-03 18:46:48 +0530

    About 2

--- my-app/src/components/About2.jsx
+++ my-app/src/components/About2.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import "./About2.css";
+import About2Img from "./About2Img.jpg";
 
 const About2 = () => {
   return (
     <div className="About2_container">
-      <h2>About 2</h2>
+      <div className="about2_contImg">
+        <div className="about2Img_container">
+          <img src={About2Img} alt="logo" className="rounded-2xl about2_img" />
+        </div>
+      </div>
+      <div className="about2_bottom">
+        <div className="about2_opportunity">
+          <h1>The Opportunity</h1>
+        </div>
+        <div className="about2_mission">
+          <h1>The Mission</h1>
+        </div>
+      </div>
     </div>
   );
 };
