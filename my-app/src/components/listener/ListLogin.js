6fc91851bca03548244eb66cfb01d74d4e5703b9
Huzefa Dohadwala <dohadwalahuzefa@gmail.com>
2023-10-15 21:53:19 +0530

    login sign up

--- my-app/src/components/listener/ListLogin.js
+++ my-app/src/components/listener/ListLogin.js
@@ -1,13 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import {
-  Box,
-  Button,
-  CircularProgress,
-  TextField,
-  Typography,
-} from "@mui/material";
 import { UserContext } from "../../UserContext";
 import { useContext } from "react";
 
@@ -47,65 +40,47 @@ const ListLogin = () => {
     }
   };
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        height: "100vh",
-      }}
+<div className="min-h-screen relative bg-[#E6E6FA]">
+    <div className="absolute bg-purple-300 rounded-full w-96 h-96 top-1 left-1 transform -translate-x-1/2 -translate-y-1/2"></div>
+    <div className="absolute bg-purple-300 rounded-full w-96 h-96 bottom-1 right-1 transform -translate-x-1 -translate-y-1.5"></div>
+  <form onSubmit={handleSubmit} className=" h-90 flex  my-24 mx-40 absolute bg-white bg-opacity-20 p-4 rounded-lg shadow-2xl backdrop-blur-md">
+  <div className="w-1/3 pt-20 content-center bg-gradient-to-r from-[#d96a94] to-[#b8a8c4] rounded-lg">
+      <h2 className="text-5xl font-bold">Welcome !!</h2>
+      <h2 className="text-5xl font-bold">Listener</h2>
+      </div>
+      <div className="w-2/3 p-4">
+    <input
+      name="email"
+      type="email"
+      placeholder="Email"
+      className="w-full p-4 m-2 border rounded-md border-gray-300 focus:ring focus:border-purple-300 focus:outline-none transition-transform transform-gpu hover:scale-105 hover:translate-y-1"
+      value={inputs.email}
+      onChange={handleChange}
+      required
+    />
+    <input
+      name="listenerPassword"
+      type="password"
+      placeholder="Password"
+      className="w-full p-4 m-2 border rounded-md border-gray-300 focus:ring focus:border-purple-300 focus:outline-none transition-transform transform-gpu hover:scale-105 hover:translate-y-1"
+      value={inputs.listenerPassword}
+      onChange={handleChange}
+      required
+    />
+    <button
+      type="submit"
+      className="w-full bg-gradient-to-r from-[#d96a94] to-[#b8a8c4] text-white font-semibold m-2 p-4 rounded-md focus:outline-none transition-transform transform-gpu hover:scale-105 hover:translate-y-1"
+      disabled={loading}
     >
-      <Typography variant="h4" gutterBottom>
-        Listener Login
-      </Typography>
-      <Box
-        component="form"
-        onSubmit={handleSubmit}
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          width: "100%",
-          maxWidth: "400px",
-        }}
-      >
-        <TextField
-          name="email"
-          type="email"
-          label="Email"
-          variant="outlined"
-          margin="normal"
-          value={inputs.email}
-          onChange={handleChange}
-          required
-        />
-        <TextField
-          name="listenerPassword"
-          type="password"
-          label="Password"
-          variant="outlined"
-          margin="normal"
-          value={inputs.listenerPassword}
-          onChange={handleChange}
-          required
-        />
-        <Button
-          type="submit"
-          variant="contained"
-          disabled={loading}
-          sx={{ mt: 3, mb: 2 }}
-        >
-          {loading ? <CircularProgress size={24} /> : "Login"}
-        </Button>
-        {error && (
-          <Typography variant="body2" color="error">
-            {error}
-          </Typography>
-        )}
-      </Box>
-    </Box>
+      {loading ? <div className="animate-spin h-5 w-5 mr-3 border-t-2 border-b-2 border-blue-500 rounded-full"></div> : "Login"}
+    </button>
+    {error && (
+      <p className="text-red-500 text-sm">{error}</p>
+    )}
+    </div>
+  </form>
+</div>
+
   );
 };
 
--- my-app/src/components/listener/ListSignup.js
+++ my-app/src/components/listener/ListSignup.js
@@ -1,4 +1,4 @@
-import { Box, Button, TextField, Typography } from "@mui/material";
+
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
@@ -9,6 +9,7 @@ const ListenerSignup = () => {
     const [inputs, setInputs] = useState({
         listenerUsername: "",
         email: "",
+        phoneNumber: "",
         listenerPassword: "",
     });
     const handleChange = (e) => {
@@ -22,60 +23,74 @@ const ListenerSignup = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const res = await axios.post("http://localhost:5000/listener/signup", inputs);
-            console.log(res);
-            navigate("/listener/login");
+          const res = await axios.post("http://localhost:5000/listener/signup", inputs);
+          console.log(res);
+          navigate("/listener/login");
         } catch (err) {
-            console.log(err);
-            setError(err.response.data.message); // set error message from server response
+          console.log(err);
+          setError(err.response.data.message); // set error message from server response
         }
-    };
+      };
 
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <Box
-                    marginLeft="auto"
-                    marginRight="auto"
-                    width={300}
-                    display="flex"
-                    flexDirection={"column"}
-                    justifyContent="center"
-                    alignItems="center"
-                >
-                    <Typography variant="h2">Listener Signup</Typography>
-                    <TextField
-                        name="listenerUsername"
-                        onChange={handleChange}
-                        value={inputs.listenerUsername}
-                        variant="outlined"
-                        placeholder="Username"
-                        margin="normal"
+        <div className="min-h-screen relative bg-[#E6E6FA]">
+            <div className="absolute bg-purple-300 rounded-full w-96 h-96 top-1 left-1 transform -translate-x-1/2 -translate-y-1/2"></div>
+            <div className="absolute bg-purple-300 rounded-full w-96 h-96 bottom-1 right-1 transform -translate-x-1 -translate-y-1.5"></div>
+
+            <form onSubmit={handleSubmit} className=" h-90 flex  my-24 mx-40 absolute bg-white bg-opacity-20 p-4 rounded-lg shadow-2xl backdrop-blur-md">
+                
+                <div className="w-1/3 pt-28 content-center bg-gradient-to-r from-[#d96a94] to-[#b8a8c4] rounded-lg">
+                <h2 className="text-5xl font-bold">Welcome !!</h2>
+                <h2 className="text-5xl font-bold">Listener</h2>
+                </div>
+                <div className="w-2/3 p-4"><input
+                    type="text"
+                    name="listenerUsername"
+                    onChange={handleChange}
+                    value={inputs.listenerUsername}
+                    placeholder="Username"
+                    className="w-full p-4 m-2 border rounded-md border-gray-300 focus:ring focus:border-purple-300 focus:outline-none transition-transform transform-gpu hover:scale-105 hover:translate-y-1"
                     />
-                    <TextField
-                        name="email"
-                        onChange={handleChange}
-                        type={"email"}
-                        value={inputs.email}
-                        variant="outlined"
-                        placeholder="Email"
-                        margin="normal"
+
+
+                    <input
+                    type="email"
+                    name="email"
+                    onChange={handleChange}
+                    value={inputs.email}
+                    placeholder="Email"
+                    className="w-full p-4 m-2 border rounded-md border-gray-300 focus:ring focus:border-purple-300 focus:outline-none transition-transform transform-gpu hover:scale-105 hover:translate-y-1"
                     />
-                    <TextField
-                        name="listenerPassword"
-                        onChange={handleChange}
-                        type="password"
-                        value={inputs.listenerPassword}
-                        variant="outlined"
-                        placeholder="Password"
-                        margin="normal"
+
+                    <input
+                    type="text"
+                    name="phoneNumber"
+                    onChange={handleChange}
+                    value={inputs.phoneNumber}
+                    placeholder="Phone Number"
+                    className="w-full p-4 m-2 border rounded-md border-gray-300 focus:ring focus:border-purple-300 focus:outline-none transition-transform transform-gpu hover:scale-105 hover:translate-y-1"
                     />
-                    <Button variant="contained" type="submit">
-                        Signup
-                    </Button>
-                    {error && <Typography color="error">{error}</Typography>}
-                </Box>
+
+                    <input
+                    type="password"
+                    name="listenerPassword"
+                    onChange={handleChange}
+                    value={inputs.listenerPassword}
+                    placeholder="Password"
+                    className="w-full p-4 m-2 border rounded-md border-gray-300 focus:ring focus:border-purple-300 focus:outline-none transition-transform transform-gpu hover:scale-105 hover:translate-y-1"
+                    />
+
+                    <button
+                    type="submit"
+                    className="w-full bg-gradient-to-r from-[#d96a94] to-[#b8a8c4] text-white font-semibold m-2 p-4 rounded-md focus:outline-none transition-transform transform-gpu hover:scale-105 hover:translate-y-1 "
+                    >
+                    Signup
+                    </button>
+                    {error && (<div className="text-red-500">{error}</div>)}
+                    </div>
             </form>
+            
+
         </div>
     );
 };
