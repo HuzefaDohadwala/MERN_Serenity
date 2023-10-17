c6244198e89fc777eb7e16cc3b524e700ac631ac
Adityaapandkar <adityaapandkar11@gmail.com>
2023-10-17 19:32:50 +0530

    therapists list initial

--- my-app/src/components/member/pages/MemTherapists.jsx
+++ my-app/src/components/member/pages/MemTherapists.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import therapistsList from "./TherapistsList.json";
+import "./MemTherapists.css";
 
 const MemTherapists = () => {
   return (
-    <div>
-      <h1>Therapists page</h1>
+    <div className="grid grid-cols-3 mt-16">
+      {therapistsList.map((therapist) => (
+        <div
+          className="TC_container bg-gray-200 rounded-lg shadow-lg p-4 mx-24 my-4"
+          key={therapist.id}
+        >
+          <div className="TherapistCard">
+            <h1>Name: {therapist.name}</h1>
+            <p>Specialization: {therapist.specialization.join(", ")}</p>
+            <p>Languages: {therapist.languages.join(", ")}</p>
+            <p>Experience: {therapist.experience}</p>
+            <button className="TC_btn">Talk Now!</button>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
