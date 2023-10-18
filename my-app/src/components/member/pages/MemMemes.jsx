bf0fb69672851ab39e85889f97d81f8872dc7c73
Huzefa Dohadwala <dohadwalahuzefa@gmail.com>
2023-10-18 19:49:10 +0530

    meme

--- my-app/src/components/member/pages/MemMemes.jsx
+++ my-app/src/components/member/pages/MemMemes.jsx
@@ -4,10 +4,13 @@ import { UserContext } from "../../../UserContext";
 import { useContext } from "react";
 
 const MemMemes = () => {
-  const [memes, setMemes] = useState(memesData.memes);
+  const initialMemes = memesData.memes.map((meme) => ({
+    ...meme,
+    showComments: false,
+  }));
+  const [memes, setMemes] = useState(initialMemes);
   const [clickedReactions, setClickedReactions] = useState({});
   const { user } = useContext(UserContext);
-  console.log(user);
 
   const handleReaction = (memeId, reactionType) => {
     if (clickedReactions[`${memeId}-${reactionType}`]) {
@@ -29,6 +32,17 @@ const MemMemes = () => {
     }
   };
 
+  const toggleComments = (memeId) => {
+    setMemes((prevMemes) =>
+      prevMemes.map((meme) => {
+        if (meme.id === memeId) {
+          return { ...meme, showComments: !meme.showComments };
+        }
+        return meme;
+      })
+    );
+  };
+
   const handleComment = (memeId, commentText) => {
     const updatedMemes = [...memes];
     const memeIndex = updatedMemes.findIndex((meme) => meme.id === memeId);
@@ -43,16 +57,16 @@ const MemMemes = () => {
   };
 
   return (
-    <div style={{ height: "100%", overflowY: "auto" }}>
+    <div style={{ height: "100%", overflowY: "auto" }} className="w-3/5  ml-52 ">
       {memes.map((meme) => (
         <div
           key={meme.id}
-          className="meme-post bg-white p-4 shadow-md rounded-lg"
+          className="meme-post bg-[#e6e6fa] pl-auto pr-auto shadow-md rounded-lg mb-8" // Adjust the card size here
         >
           <img
             src={meme.image}
             alt={meme.title}
-            className="w-24 h-24 rounded-lg"
+            className="w-3/4 pl-36 pt-4 h-auto "
           />
           <div className="reactions mt-2 flex space-x-4">
             <button
@@ -60,35 +74,52 @@ const MemMemes = () => {
               className="text-blue-500 hover:text-blue-700"
               disabled={clickedReactions[`${meme.id}-like`]}
             >
-              Like ({meme.reactions.like})
+              👍 {meme.reactions.like}
             </button>
             <button
               onClick={() => handleReaction(meme.id, "love")}
               className="text-red-500 hover:text-red-700"
               disabled={clickedReactions[`${meme.id}-love`]}
             >
-              Love ({meme.reactions.love})
+              ❤️ {meme.reactions.love}
+            </button>
+            <button
+              onClick={() => handleReaction(meme.id, "wow")}
+              className="text-yellow-500 hover:text-yellow-700"
+              disabled={clickedReactions[`${meme.id}-wow`]}
+            >
+              😲 {meme.reactions.wow}
             </button>
           </div>
-          <div className="comments mt-4">
-            {meme.comments.map((comment, index) => (
-              <div key={index} className="comment bg-gray-100 p-2 rounded">
-                <p>{comment.text}</p>
-                <span className="text-gray-500">{comment.user}</span>
-              </div>
-            ))}
-          </div>
-          <input
-            type="text"
-            placeholder="Add a comment..."
-            onKeyUp={(e) => {
-              if (e.key === "Enter") {
-                handleComment(meme.id, e.target.value);
-                e.target.value = "";
-              }
-            }}
-            className="mt-4 p-2 border rounded w-full focus:outline-none focus:border-blue-500"
-          />
+          <button
+            onClick={() => toggleComments(meme.id)}
+            className="mt-2 text-black font-semibold hover:text-blue-700"
+          >
+            {meme.showComments ? "Hide Comments" : "Show Comments"}
+          </button>
+          {meme.showComments && (
+            <div className="comments mt-4">
+              {meme.comments.map((comment, index) => (
+                <div key={index} className="comment bg-gray-100 p-2 rounded">
+                  <p>{comment.text}</p>
+                  <span className="text-gray-500">{comment.user}</span>
+                </div>
+              ))}
+            </div>
+          )}
+          {meme.showComments && (
+            <input
+              type="text"
+              placeholder="Add a comment..."
+              onKeyUp={(e) => {
+                if (e.key === "Enter") {
+                  handleComment(meme.id, e.target.value);
+                  e.target.value = "";
+                }
+              }}
+              className="mt-4 p-2 border rounded w-full focus:outline-none focus:border-blue-500"
+            />
+          )}
         </div>
       ))}
     </div>
