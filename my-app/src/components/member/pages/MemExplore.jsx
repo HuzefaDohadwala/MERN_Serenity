1cd9fad9f434bc3ec5f8088e676c6cf0462fe2c0
Huzefa Dohadwala <dohadwalahuzefa@gmail.com>
2023-10-19 02:35:11 +0530

    exp

--- my-app/src/components/member/pages/MemExplore.jsx
+++ my-app/src/components/member/pages/MemExplore.jsx
@@ -5,14 +5,33 @@ import { UserContext } from "../../../UserContext";
 
 const MemExplore = () => {
   const [events, setEvents] = useState([]);
+  const [selectedEvent, setSelectedEvent] = useState(null);
   const { user } = useContext(UserContext);
   // console.log(user);
   const [formData, setFormData] = useState({
     time: "",
     username: user.user.username,
     email: user.user.email,
+    company: selectedEvent ? selectedEvent.company : "",
+    topic: selectedEvent ? selectedEvent.topic : "",
   });
 
+  const handleEventSelect = (event) => {
+    setSelectedEvent(event);
+  
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [event._id]: {
+        time: "",
+        username: user.user.username,
+        email: user.user.email,
+        company: event.company,
+        topic: event.topic,
+      },
+    }));
+  };
+  
+  
   useEffect(() => {
     const fetchEvents = async () => {
       try {
@@ -31,10 +50,17 @@ const MemExplore = () => {
     fetchEvents();
   }, []);
 
-  const handleChange = (e) => {
+  const handleChange = (e, eventId) => {
     const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [eventId]: {
+        ...prevFormData[eventId],
+        [name]: value,
+      },
+    }));
   };
+  
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -48,6 +74,8 @@ const MemExplore = () => {
           time: currentTime,
           email: formData.email,
           username: formData.username,
+          company: formData.company,
+          topic: formData.topic,
         },
       }),
     })
@@ -69,6 +97,7 @@ const MemExplore = () => {
         <div
           key={event._id}
           className="bg-[#7e506c] p-4 rounded-lg shadow-lg m-4 max-w-screen flex"
+          onClick={() => handleEventSelect(event)}
         >
           <div className="flex-shrink-0">
             <img
@@ -78,7 +107,7 @@ const MemExplore = () => {
             />
           </div>
           <div className="ml-4 flex-grow">
-            <h2 className="text-2xl font-semibold text-white">{event.name}</h2>
+            <h2 className="text-2xl font-semibold text-white">{event.company}</h2>
             <p className=" text-left text-white">{event.date}</p>
             <p className=" text-left text-white">{event.location}</p>
             <div className="mt-4 text-right">
@@ -114,6 +143,26 @@ const MemExplore = () => {
                       value={formData.email}
                       onChange={handleChange}
                     />
+                  </div>
+                  <div>
+                    <input
+                    type="text"
+                    id="company"
+                    name="company"
+                    value={formData[event._id]?.company || ""}
+                    onChange={(e) => handleChange(e, event._id)}
+                    disabled
+                  />
+                  </div>
+                  <div>
+                  <input
+                    type="text"
+                    id="topic"
+                    name="topic"
+                    value={formData[event._id]?.topic || ""}
+                    onChange={(e) => handleChange(e, event._id)}
+                    disabled
+                  />
                   </div>
                   <div>
                     <button type="submit">Submit</button>
