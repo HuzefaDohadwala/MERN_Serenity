29e7560a53aa85b43f4dbb64184564df2dad50d9
Huzefa Dohadwala <dohadwalahuzefa@gmail.com>
2023-10-18 22:08:27 +0530

    excel

--- my-app/src/components/member/pages/MemExplore.jsx
+++ my-app/src/components/member/pages/MemExplore.jsx
@@ -1,8 +1,43 @@
 import React from "react";
 import explore from './Explore.json';
 import './Explore.css'
+import { useState } from 'react';
 
 const MemExplore = () => {
+  const [formData, setFormData] = useState({
+    time: '',
+    username: '',
+    email: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch("https://api.apispreadsheets.com/data/DPASwoFmLyBXwDUW/", {
+      method: "POST",
+      body: JSON.stringify({
+        data: {
+          time: formData.time,
+          email: formData.email,
+          username: formData.username,
+        },
+      }),
+    })
+      .then((res) => {
+        if (res.status === 201) {
+          // SUCCESS
+        } else {
+          // ERROR
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
   return (
     <div style={{ height: "100%", overflowY: "auto" }}>
     {explore.map((event, index) => (
@@ -15,9 +50,44 @@ const MemExplore = () => {
           <p className=" text-left text-white">{event.date}</p>
           <p className=" text-left text-white">{event.location}</p>
           <div className="mt-4 text-right">
-            <button className="bg-gradient-to-r from-[#d96a94] to-[#b8a8c4] text-white font-semibold py-2 px-4 rounded focus:outline-none focus:ring focus:border-blue-300 hover:bg-gradient-to-r hover:from-[#b8a8c4] hover:to-[#d96a94] transform hover:scale-105">
-              Register
-            </button>
+          <div>
+      <h2>User Form</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="time">Time:</label>
+          <input
+            type="text"
+            id="time"
+            name="time"
+            value={formData.time}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
           </div>
         </div>
       </div>
